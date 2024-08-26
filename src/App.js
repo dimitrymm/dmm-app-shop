990be8cfd9a47bbe00698208fa81c41ed7cfaca6
Dimitry <dimitrymmarinho@gmail.com>
2024-08-26 10:15:27 -0300

    feat: :art: Criação Estrutura
    Criando os escopos dos componentes para criação da logica

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [productName, setProductName] = useState("");
+  const [productDateBuy, setProductDateBuy] = useState("");
+  const [productQuantity, setProductQuantity] = useState("");
+  const [productCategory, setProductCategory] = useState("");
+
+  function handleAddProduct(event) {
+    event.preventDefault();
+    console.table({
+      productDateBuy,
+      productQuantity,
+      productName,
+      productCategory,
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="">
+      <header className="flex justify-center">
+        <h1>Adicione sua compra</h1>
       </header>
+      <main className="bg-slate-400 m-10 p-2">
+        <form>
+          <input
+            value={productDateBuy}
+            onChange={(event) => setProductDateBuy(event.target.value)}
+            type="date"
+          />
+          <input
+            value={productQuantity}
+            onChange={(event) => setProductQuantity(event.target.value)}
+            placeholder="Quantidade"
+          />
+          <input
+            value={productName}
+            onChange={(event) => setProductName(event.target.value)}
+            placeholder="Nome"
+          />
+          <select
+            value={productCategory}
+            onChange={(event) => setProductCategory(event.target.value)}
+          >
+            <option value="">Categoria</option>
+            <option value="frutas">Frutas</option>
+            <option value="legumes">Legumes</option>
+          </select>
+          <button className="" type="submit" onClick={handleAddProduct}>
+            Adicionar Compra
+          </button>
+        </form>
+      </main>
+      <aside className="bg-slate-400 m-10 p-2">
+        <h1 className="text-center">Compras Listadas</h1>
+
+        <div>
+          <p>Data: {productDateBuy}</p>
+          <p>Quantidade: {productQuantity}</p>
+          <p>Nome: {productName}</p>
+          <p>Categoria: {productCategory}</p>
+        </div>
+      </aside>
     </div>
   );
 }
